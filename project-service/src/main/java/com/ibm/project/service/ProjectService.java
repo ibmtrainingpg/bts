a818e71b6a6685bb46ecd6597f598460e098864d
unknown <ibmjavanh19@iiht.tech>
2021-03-30 20:55:32 +0530

    changes

--- project-service/src/main/java/com/ibm/project/service/ProjectService.java
+++ project-service/src/main/java/com/ibm/project/service/ProjectService.java
@@ -3,9 +3,12 @@ package com.ibm.project.service;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.ibm.project.entity.Project;
 import com.ibm.project.repo.ProjectRepository;
 
+@Service
 public class ProjectService {
 	@Autowired
 	ProjectRepository projectRepository;
