b5e53c82c23037ed4d6e31f786b9076f2087b961
unknown <ibmjavanh19@iiht.tech>
2021-04-01 00:31:45 +0530

    resolved issues related to valid annotation

--- bug-service/src/main/java/com/ibm/bug/BugController.java
+++ bug-service/src/main/java/com/ibm/bug/BugController.java
@@ -52,7 +52,7 @@ public class BugController {
 	
 	@PutMapping("/bug/{id}")
 	void updateBugStatus(@RequestBody @Valid Bug bug, BindingResult bindingResult,@PathVariable("id") String bugId ) {
-//		System.out.println(bugId);
+		System.out.println(bugId);
 		bug.setId(bugId);
 		bugService.updateBugStatus(bug);	
 	}
