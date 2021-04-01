ab95e3f2ac438781da45177929f2c641c125babd
ibmabhishek <erren98@gmail.com>
2021-04-01 14:26:22 +0530

    Implemented GET in employee and renamed method in BugController

--- employee-service/src/main/java/com/ibm/employee/EmployeeController.java
+++ employee-service/src/main/java/com/ibm/employee/EmployeeController.java
@@ -1,8 +1,14 @@
 package com.ibm.employee;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ibm.employee.entity.Employee;
 import com.ibm.employee.service.EmployeeService;
 
 
@@ -10,5 +16,17 @@ import com.ibm.employee.service.EmployeeService;
 public class EmployeeController {
 	@Autowired
 	EmployeeService employeeService;
+	
+	@GetMapping("/employee")
+	List<Employee> getEmployees() {
+		return employeeService.getEmployees();
+
+	}
+	
+	@GetMapping("/employee/{id}")
+	Optional<Employee> getBug(@PathVariable("id") String employeeId) {
+		return employeeService.getEmployee(employeeId);
+	}
+
 	
 }
