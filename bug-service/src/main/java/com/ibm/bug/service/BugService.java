9f273f219157f5f3faa14d5bcebbd5187dd3b4bd
ibmabhishek <erren98@gmail.com>
2021-04-01 11:56:22 +0530

    Implemented updateBugStatus JUNIT Testcase

--- bug-service/src/main/java/com/ibm/bug/service/BugService.java
+++ bug-service/src/main/java/com/ibm/bug/service/BugService.java
@@ -3,11 +3,13 @@ package com.ibm.bug.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.net.ssl.SSLEngineResult.Status;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ibm.bug.STATUS;
 import com.ibm.bug.entity.Bug;
 import com.ibm.bug.repo.BugRepository;
 
@@ -36,8 +38,15 @@ public class BugService {
 		return bugRepository.findById(bugId);
 	}
 	
-	public void updateBugStatus(@Valid Bug bug) {
-		 bugRepository.save(bug);
+	/**
+	 * method to update the status of the code
+	 * @param bug
+	 * @returns updated bug status
+	 */
+	
+	public STATUS updateBugStatus(@Valid Bug bug) {
+		bugRepository.save(bug);
+		return bug.getStatus();
 		
 	}
 	public String createBug(@Valid Bug bug) {
--- bug-service/src/test/java/com/ibm/bug/service/BugServiceTest.java
+++ bug-service/src/test/java/com/ibm/bug/service/BugServiceTest.java
@@ -2,10 +2,14 @@ package com.ibm.bug.service;
 
 
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import javax.net.ssl.SSLEngineResult.Status;
 
 import org.junit.jupiter.api.Test;
 
+import com.ibm.bug.STATUS;
 import com.ibm.bug.entity.Bug;
 import com.ibm.bug.repo.BugRepository;
 
@@ -26,10 +30,16 @@ class BugServiceTest {
 //		fail("Not yet implemented");
 //	}
 //
-//	@Test
-//	void testUpdateBugStatus() {
-//		fail("Not yet implemented");
-//	}
+	@Test
+	void testUpdateBugStatus() {
+		BugService bugService = new BugService();
+		BugRepository dummyRepo = new DummyBugRepository();
+		bugService.setBugRepository(dummyRepo);
+		Bug bug = new Bug();
+		STATUS status = bugService.updateBugStatus(bug);
+		assertNotNull(status);
+		assertEquals(STATUS.VERIFIED, status);
+	}
 //
 //	@Test
 //	void testCreateProject() {
