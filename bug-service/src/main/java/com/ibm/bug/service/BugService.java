a5482d87b614b7f2309fcbf729e7dca5a99e5f47
unknown <ibmjavanh19@iiht.tech>
2021-04-01 11:48:07 +0530

    -resolved errors in bug service and pom

--- bug-service/src/main/java/com/ibm/bug/service/BugService.java
+++ bug-service/src/main/java/com/ibm/bug/service/BugService.java
@@ -2,12 +2,9 @@ package com.ibm.bug.service;
 
 import java.util.List;
 import java.util.Optional;
-
 import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.ibm.bug.entity.Bug;
 import com.ibm.bug.repo.BugRepository;
 
