cc055acd8e0e8982b65c0bcd2b653182a18efaf3
ibmabhishek <erren98@gmail.com>
2021-04-01 15:53:54 +0530

    Added parenthesis in methods

--- employee-service/src/main/java/com/ibm/employee/service/EmployeeService.java
+++ employee-service/src/main/java/com/ibm/employee/service/EmployeeService.java
@@ -17,6 +17,7 @@ public class EmployeeService {
 
 	public void updateEmployeeDetails(@Valid Employee employee) {
 		employeeRepository.save(employee);
+	}
 	/**
 	 * 
 	 * @param employee
@@ -25,12 +26,14 @@ public class EmployeeService {
 	public String createEmployee(@Valid Employee employee) {
 		Employee savedemployee=employeeRepository.save(employee);
 		return employee.getId();
+	}
 
 	/**
 	 * service method to get details of the specific employee
 	 * @param employeeId
 	 * @return zero or matching employee details
 	 */
+	
 	public Optional<Employee> getEmployee(String employeeId) {
 		return employeeRepository.findById(employeeId);
 	}
