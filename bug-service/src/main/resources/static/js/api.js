8d763c70ffb42e3ebd2cffe5389b7199770f17ac
ibmabhishek <erren98@gmail.com>
2021-04-09 20:58:46 +0530

    Added the Get Method for the Bugs

--- bug-service/src/main/resources/static/js/api.js
+++ bug-service/src/main/resources/static/js/api.js
@@ -40,3 +40,98 @@ function saveBug() {
     promise.catch(errorHandler);
 
 }
+//=================GetBugs Method=====================================
+function getBugs() {
+    const promise = fetch('/bug');
+    promise.then(function (response) {
+        return response.json();
+    })
+        .then(function (bugs) {
+            console.log(bugs);
+            const table = document.getElementById('bugsTable');
+            for (let index = 0; index < bugs.length; index++) {
+                console.log(bugs[index]);
+                const currentBug = bugs[index];
+                const row = document.createElement('tr');
+                const titleColumn = document.createElement('td');
+                const descriptionColumn = document.createElement('td');
+                const statusColumn = document.createElement('td');
+                const priorityColumn = document.createElement('td');
+                const typeColumn = document.createElement('td');
+                const submitOnDateColumn = document.createElement('td');
+                const buildVersionColumn = document.createElement('td');
+                const projectIdColumn = document.createElement('td');
+                const moduleColumn = document.createElement('td');
+                const productColumn = document.createElement('td');
+
+                titleColumn.append(currentBug.title);
+                descriptionColumn.append(currentBug.description);
+                statusColumn.append(currentBug.status);
+                priorityColumn.append(currentBug.priority);
+                typeColumn.append(currentBug.type);
+                submitOnDateColumn.append(currentBug.submitOnDate);
+                buildVersionColumn.append(currentBug.buildVersion);
+                projectIdColumn.append(currentBug.projectId);
+                moduleColumn.append(currentBug.module);
+                productColumn.append(currentBug.product);
+                row.appendChild(titleColumn);
+                row.appendChild(descriptionColumn);
+                row.appendChild(statusColumn);
+                row.appendChild(priorityColumn);
+                row.appendChild(typeColumn);
+                row.appendChild(submitOnDateColumn);
+                row.appendChild(buildVersionColumn);
+                row.appendChild(projectIdColumn);
+                row.appendChild(moduleColumn);
+                row.appendChild(projectIdColumn);
+                table.appendChild(row);
+            }
+        })
+}
+
+//====================GetBugsById=====================================
+function getBug() {
+    let id = document.getElementById('bugId');
+    const promise = fetch('/bug/' + id);
+    promise.then(function (response) {
+        return response.json();
+    })
+        .then(function (bug) {
+            console.log(bug);
+            const table = document.getElementById('bugTable');
+            const row = document.createElement('tr');
+            const titleColumn = document.createElement('td');
+            const descriptionColumn = document.createElement('td');
+            const statusColumn = document.createElement('td');
+            const priorityColumn = document.createElement('td');
+            const typeColumn = document.createElement('td');
+            const submitOnDateColumn = document.createElement('td');
+            const buildVersionColumn = document.createElement('td');
+            const projectIdColumn = document.createElement('td');
+            const moduleColumn = document.createElement('td');
+            const productColumn = document.createElement('td');
+
+            titleColumn.append(bug.title);
+            descriptionColumn.append(bug.description);
+            statusColumn.append(bug.status);
+            priorityColumn.append(bug.priority);
+            typeColumn.append(bug.type);
+            submitOnDateColumn.append(bug.submitOnDate);
+            buildVersionColumn.append(bug.buildVersion);
+            projectIdColumn.append(bug.projectId);
+            moduleColumn.append(bug.module);
+            productColumn.append(bug.product);
+            row.appendChild(titleColumn);
+            row.appendChild(descriptionColumn);
+            row.appendChild(statusColumn);
+            row.appendChild(priorityColumn);
+            row.appendChild(typeColumn);
+            row.appendChild(submitOnDateColumn);
+            row.appendChild(buildVersionColumn);
+            row.appendChild(projectIdColumn);
+            row.appendChild(moduleColumn);
+            row.appendChild(projectIdColumn);
+            table.appendChild(row);
+        })
+}
+
